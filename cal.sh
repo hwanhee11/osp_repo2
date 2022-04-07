239a6cf3b7b42536b3b7ecb3192dab41c4328446
hwanhee11 <qgg1978@naver.com>
2022-04-07 19:11:31 +0900

    cal.sh

---
+++ cal.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+num1=$(<num1.txt)
+num2=$(<num2.txt)
+menu=$1
+
+if [[ "$menu" = "add" ]]; then
+    echo -e "\nnum1 : $num1"; echo "num2 : $num2"; echo "op : $menu"
+    let result=$num1+$num2; echo "result : $result"; exit 1
+elif [[ "$menu" = "sub" ]]; then
+    echo -e "\nnum1 : $num1"; echo "num2 : $num2"; echo "op : $menu"
+    let result=$num1-$num2; echo "result : $result"; exit 1
+elif [[ "$menu" = "div" ]]; then
+    echo -e "\nnum1 : $num1"; echo "num2 : $num2"; echo "op : $menu"
+    let result=$num1/$num2; echo "result : $result"; exit 1
+elif [[ "$menu" = "mul" ]]; then
+    echo -e "\nnum1 : $num1"; echo "num2 : $num2"; echo "op : $menu"
+    let result=$num1*$num2; echo "result : $result"; exit 1
+else
+    echo "...none operator parameter..."
+fi
+
+PS3='select menu : '
+select menu in "add" "sub" "div" "mul"
+do
+    if [[ "$menu" = "add" ]]; then
+        echo -e "\nnum1 : $num1"; echo "num2 : $num2"; echo "op : $menu"
+        let result=$num1+$num2; echo "result : $result"; exit 1
+    elif [[ "$menu" = "sub" ]]; then
+        echo -e "\nnum1 : $num1"; echo "num2 : $num2"; echo "op : $menu"
+        let result=$num1-$num2; echo "result : $result"; exit 1
+    elif [[ "$menu" = "div" ]]; then
+        echo -e "\nnum1 : $num1"; echo "num2 : $num2"; echo "op : $menu"
+        let result=$num1/$num2; echo "result : $result"; exit 1
+    elif [[ "$menu" = "mul" ]]; then
+        echo -e "\nnum1 : $num1"; echo "num2 : $num2"; echo "op : $menu"
+        let result=$num1*$num2; echo "result : $result"; exit 1
+    fi
+done
